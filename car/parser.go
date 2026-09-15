252eec96a33bef5bf36f08b52b02d9c26869f75d
Michael Woods <michaelthomaswoods@gmail.com>
2026-09-15 01:20:40 -0700

    generalized iAP parser

--- car/parser.go
+++ car/parser.go
@@ -5,6 +5,11 @@ import (
 	"machine"
 )
 
+const (
+	SyncByte        byte = 0xFF
+	PacketStartByte byte = 0x55
+)
+
 // Configure the hardware UART interface
 var Serial = machine.UART0
 
@@ -15,47 +20,54 @@ func InitializeSerialBus() error {
 	return nil
 }
 
+// Packet Details  The sync byte (OxFF) is not considered part of the packet.
+// It is sent merely to facilitate automatic baud rate  detection and correction when using a UART serial port link and, in some cases, to power on the iPod. It is  not necessary to send the sync byte when using BT or USB as a link.
+// The packet payload length is the number of bytes in the packet, not including the sync byte, packet start  byte, packet payload length byte, or packet payload checksum byte. That is, it is the length of the command  ID, lingo, and command data. Thus, the packet payload data length for a Request Identify command  would be 0x02. The Lingo ID specifies the broad category that the communication falls under.
+// The Command  ID is a more specific indication of the significance of the packet and is interpreted differently depending on  the Lingo ID.
+
 // ReadCarPacket scans the incoming UART buffer look for valid iAP protocol patterns
 func ReadCarPacket() (byte, bool) {
 	// 1. Look for the header sequence by shifting out dead bytes
-	for Serial.Buffered() >= 8 {
-		b1, _ := Serial.ReadByte()
-		print(b1)
-		if b1 != 0x55 {
-			println("[CAR_ERR] Bad packet b1 (Not 0x55)")
+	for Serial.Buffered() >= 7 {
+		sync, _ := Serial.ReadByte()
+		if sync != SyncByte {
+			println("[CAR_ERR] Bad Sync Byte (Not 0xFF)")
 			continue // Skip this junk byte and check the next one
 		}
-		// Look ahead at the next byte without removing it yet
-		b2, _ := Serial.ReadByte()
-		print(b2)
-		if b2 != 0xAA {
-			println("[CAR_ERR] Bad packet b2 (Not 0xAA)")
-			// Not a match! But b2 might be 0x55, so check again next loop
-			continue
+		b0, _ := Serial.ReadByte()
+		if b0 == SyncByte {
+			println("[CAR_INFO] Second Sync Byte (0xFF)")
+			b0, _ = Serial.ReadByte()
 		}
 
-		// 2. Read the fixed 8 bytes into a temporary buffer to preserve them for logging
-		packetBuf := make([]byte, 8)
-		packetBuf[0] = b1
-		packetBuf[1] = b2
-		for i := 2; i < 8; i++ {
+		if b0 != PacketStartByte {
+			println("[CAR_ERR] Bad SOP Byte (Not 0x55)")
+			continue // Skip this junk byte and check the next one
+		}
+
+		payloadLength, _ := Serial.ReadByte()
+
+		// 2. Read the bytes into a temporary buffer to preserve them for logging
+		lenBuf := 3 + int(payloadLength) // packet start byte + length byte + checksum byte + payloadLength
+		packetBuf := make([]byte, lenBuf)
+		packetBuf[0] = b0
+		packetBuf[1] = payloadLength
+		for i := 2; i < lenBuf; i++ {
 			packetBuf[i], _ = Serial.ReadByte()
+		}
+		checksum := packetBuf[lenBuf-1]
+
+		sum := int(payloadLength)
+		for i := 2; i < lenBuf-1; i++ {
+			sum += int(packetBuf[i])
 		}
 
 		// 3. IMMEDIATELY LOG THE RAW HEX (Crucial for debugging/research)
 		// Output format example: [UART_RAW] 55 aa 03 02 00 00 01 fa
 		println("[UART_RAW]", hex.EncodeToString(packetBuf))
 
-		// 4. Map variables to our safe buffer fields
-		length := packetBuf[2]
-		mode := packetBuf[3]
-		cmdH := packetBuf[4]
-		cmdL := packetBuf[5]
-		data := packetBuf[6]
-		checksum := packetBuf[7]
-
 		// 6. Simple Checksum Validation
-		calcChecksum := byte(0x100 - (int(length)+int(mode)+int(cmdH)+int(cmdL)+int(data))&0xFF)
+		calcChecksum := byte(0x100 - (sum)&0xFF)
 
 		if calcChecksum != checksum {
 			println("[CAR_ERR] Bad packet checksum match dropped. Expected:", calcChecksum, "Got:", checksum)
@@ -63,9 +75,9 @@ func ReadCarPacket() (byte, bool) {
 		}
 
 		// 7. Verify it is a true Mode 2 button event
-		if mode == 0x02 && cmdH == 0x00 && cmdL == 0x00 {
-			return data, true
-		}
+		// if mode == 0x02 && cmdH == 0x00 && cmdL == 0x00 {
+		// 	return data, true
+		// }
 	}
 
 	// println("[CAR_INF] Valid iAP packet received, but not a Mode 2 button event")
